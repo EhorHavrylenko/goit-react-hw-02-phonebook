43691536429e79265fc1958c1a019fd0fdd74be3
Egor Bass <egorbass@MBP-Egor.N1>
2021-03-03 13:46:54 +0200

    fix key

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FeedBackOptions.module.css';
+import shortid from 'shortid';
 
 const options = [ 'good', 'neutral', 'bad' ];
 
-const FeedbackButton = ({ feedback, onLeaveFeedback }) => {
+const FeedbackButton = ({ feedback, onLeaveFeedback, key }) => {
 	return (
-		<button className={styles.btn} type="button" data-feedback={feedback} onClick={onLeaveFeedback}>
+		<button key={key} className={styles.btn} type="button" data-feedback={feedback} onClick={onLeaveFeedback}>
 			{feedback}
 		</button>
 	);
 };
 
 const FeedbackOptions = ({ onLeaveFeedback }) => {
-	return options.map((option) => FeedbackButton({ feedback: option, onLeaveFeedback }));
+	return options.map((option) => FeedbackButton({ key: shortid.generate(), feedback: option, onLeaveFeedback }));
 };
 
 FeedbackOptions.propTypes = {
